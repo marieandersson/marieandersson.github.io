452bb6f7c33744567eee804f0c46ac23322508cd
Marie Andersson <marie.andersson2303@gmail.com>
2018-11-16 16:08:50 +0100

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,89 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
+    <p class="desc">This is my css <span class="playground" v-for="letter in letters">{{letter}}</span>.</p>
+    <Bubble />
   </div>
 </template>
 
 <script>
+import Bubble from './components/Bubble.vue'
 export default {
   name: 'app',
+  components: {
+    Bubble
+  },
   data () {
     return {
-      msg: 'Hello. I\'m Marie, and this is about to become my awesome Github page.'
+      title: 'Hello. I\'m Marie.',
+      letters: ['p', 'l', 'a', 'y', 'g', 'r', 'o', 'u', 'n', 'd']
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+@keyframes jump {
+  0% {
+    transform: translateY(0) scale(1);
+  }
+  10% {
+    transform: translateY(-5px) scale(1.1);
+  }
+  18% {
+    transform: translateY(0) scale(1);
+  }
+  100% {
+    transform: translateY(0) scale(1);
+  }
+}
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #000;
-  margin-top: 60px;
+  padding: 50px;
+  position: relative;
+  height: 100%;
 }
 
-h1, h2 {
+h1 {
   font-weight: normal;
+  position: relative;
+
+  &::after {
+    width: 250px;
+    position: absolute;
+    left: 0;
+    content: '';
+    height: 20px;
+    background: #000;
+    opacity: .3;
+    transform: translate(15px, 22px) skewX(10deg);
+  }
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.desc {
+  font-size: 1.2rem;
+  letter-spacing: 1.2px;
+
+  .playground {
+    display: inline-block;
+
+    @for $i from 1 through 10 {
+      &:nth-child(#{$i})  {
+
+        animation: jump 3s ease-in;
+        animation-iteration-count: infinite;
+        animation-delay:  #{$i * .1}s;
+        animation-timing-function: ease;
+      }
+
+    }
+  }
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Bubble.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="bubble"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Bubble',
+  data() {
+    return {
+      isHovering: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.bubble {
+  position: absolute;
+  left: 10%;
+  top: 30%;
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  background-color: #000;
+  -webkit-box-shadow: 0px 20px 55px -9px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 20px 55px -9px rgba(0,0,0,0.75);
+  box-shadow: 0px 20px 55px -9px rgba(0,0,0,0.75);
+}
+</style>
